de3924c80fe493314d149fd73d5f00701286deda
David Veele <you@example.com>
2020-06-03 21:56:50 -0400

    first 3 questions in js answered

--- index.js
+++ index.js
@@ -28,10 +28,17 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ Th difference between count one and count two is scope. In count1 it will only work in the function itself, 
+ in count2 the cariable count can be used throughout the whole code.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * counter one is the one that uses closure. It has access to the parent function whereas counter two is only a parent function
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * 
+ *counter1 would me more useful for code blocks that need many steps or return values once, counter2 would be more 
+ useful when you have to call the information many times in different areas of the code.
 */
 
 // counter1 code
@@ -54,17 +61,20 @@ function counter2() {
 
 /* Task 2: inning() 
 
-Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
+Write a function called `inning` that generates a random number of points that a team scored in an inning.
+ This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+ function inning(min,max){
+  let score = Math.floor(Math.random() * 3);
+  return score;
 }
+console.log (inning(0, 3));
+
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a 
+number of innings and and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -76,12 +86,23 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inning, number){
+  let homeTeam = 0;
+  let awayTeam = 0;
+  for (let i = 0; i < number; i ++){
+    homeTeam = homeTeam + inning();
+    awayTeam = awayTeam + inning();
+  } 
+  return{
+    "Home" : homeTeam,
+    "Away" : awayTeam,
+  }
 }
 
+console.log(finalScore(inning, 9));
+console.log(finalScore(inning, 9));
+console.log(finalScore(inning, 9));
+console.log(finalScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
